657f3437ba4f8a6d3fc517b436f9885f85c1249f
jibinshan <jibinshanu23@gmail.com>
2025-05-24 00:12:46 +0530

    changes in table booking and hero video

--- src/app/table-booking/(section)/TableBookingN.tsx
+++ src/app/table-booking/(section)/TableBookingN.tsx
@@ -3,7 +3,7 @@ const TableBookingN = ({}) => {
     <section className="h-fit w-full">
       <iframe
         src="https://web.dojo.app/create_booking/vendor/AUTSb3_q5k-n8fH8VZbfJ7VmxjgusGCENGoprHUelyA_restaurant"
-        className="h-[500px] w-full"
+        className="h-[1300px] w-full"
       ></iframe>
     </section>
   );
